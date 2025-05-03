fdbf7f648f0f6f841e907c1ddfdaa31fad4e9901
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-05-03 23:37:42 +0530

    removed redudent apis

--- src/lib/service/auth.ts
+++ src/lib/service/auth.ts
@@ -1,8 +1,6 @@
 import {KF_CLIENT_ID,KF_CLIENT_SECRET} from "@/lib/config/envConfig";
 import ENDPOINT from "@/assets/data/api";
-import { kfBackendClient,resolveHttpError,resolveHttpResponse } from "../utils/httpUtil";
-import axios, { AxiosResponse } from "axios";
-import ResponseData from "@/lib/type/ResponseData";
+import { kfBackendClient } from "../utils/httpUtil";
 
 /**
  * 
@@ -28,44 +26,5 @@ export async function authorizeClient(){
     } catch (error) {
         console.log(error)
         return false;
-    }
-}
-
-/**
- * 
- * @param username 
- * @param password 
- */
-export const loginAction = async (username:String, password: String) => {
-    let data : ResponseData;
-    try {
-        const body = {
-            username,
-            password
-        }
-
-        const response : AxiosResponse<any, any> = await axios.post(ENDPOINT.LOGIN_ACTION_API,body);
-        data = resolveHttpResponse(response);
-        
-    } catch (error: any) {
-        data = resolveHttpError(error);
-    }
-
-    return data;
-}
-
-/**
- * 
- * @param token 
- * @param grantType 
- * @returns 
- */
-export const exchangeForToken = async (token:String, grantType:String) => {
-    
-    try{
-        const response : AxiosResponse<any,any> = await kfBackendClient.post(ENDPOINT.TOKEN_CLIENT_API,{token,grantType});
-        return resolveHttpResponse(response);
-    }catch(error : any){
-        return resolveHttpError(error);
     }
 }
